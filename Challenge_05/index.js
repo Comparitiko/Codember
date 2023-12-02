751b9472d38e12f4fd4936d215bd39bf7953c73e
Comparitiko <gabigcy@gmail.com>
2023-12-02 12:56:50 +0100

    Haciendo challenge_05

--- Challenge_05/index.js
+++ Challenge_05/index.js
@@ -3,17 +3,20 @@ const archivo = await readFile(`${process.cwd()}/Challenge_05/database-attacked.
 const usuarios = archivo.split('\n')
 let res = ''
 const abecedario = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('')
+console.log(abecedario)
 const numeros = '1234567890'.split('')
+console.log(numeros)
 
 // Funcion que devuelve la primera letra del userName
 function sacarPrimeraLetra (usuario) {
   const datosUsuario = usuario.split(',')
-  return datosUsuario[0].chartAt(0)
+  return datosUsuario[1].chartAt(0)
 }
 
 function idValido (id) {
   const letras = id.split('')
   for (const letra of letras) {
+    // console.log(`Letra o numero ${letra} en diccionario: ${(letra in abecedario) || (letra in numeros)}`)
     if (!(letra in abecedario) || !(letra in numeros)) return false
   }
   return true
@@ -30,25 +33,28 @@ function emailValido (email) {
 }
 
 function edadValidaONula (edad) {
-  const digitosEdad = edad.split('-')
+  const digitosEdad = edad.split('')
   for (const digitoEdad of digitosEdad) {
-    if (digitoEdad !== '' || !(digitoEdad in numeros)) return false
+    if (digitoEdad !== '' || !(digitoEdad in numeros)) return true
   }
-  return true
+  return false
 }
 
-function localidadValidaoNula (localidad) {
+function localidadValidaONula (localidad) {
   if (typeof id === 'string' || '') return true
   else return false
 }
 
 function esUsuarioValido (usuario) {
   const datosUsuario = usuario.replace('\r', '').split(',')
+  idValido(datosUsuario[0])
   // console.log(idValido(datosUsuario[0]))
   // console.log(nombreUsuarioValido(datosUsuario[1]))
   // console.log(emailValido(datosUsuario[2]))
-  console.log(edadValidaONula(datosUsuario[3]))
+  // console.log(edadValidaONula(datosUsuario[3]))
   // console.log(localidadValidaoNula(datosUsuario[4]))
+  if (idValido(datosUsuario[0]) && nombreUsuarioValido(datosUsuario[1]) && emailValido(datosUsuario[2]) && edadValidaONula(datosUsuario[3]) && localidadValidaONula(datosUsuario[4])) return true
+  else return false
 }
 
 for (const usuario of usuarios) {
